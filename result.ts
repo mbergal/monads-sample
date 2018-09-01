8ab68bc1bf3fb164a43b9a956e5b045e6495979d
Misha Bergal <misha.bergal@gmail.com>
2018-09-01 12:13:12 -0500

    WIP

--- result.ts
+++ result.ts
@@ -1,51 +1,43 @@
-namespace Monads {
-  abstract class Result<A, E> {
-    abstract bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E>;
-    abstract toString(): string;
+abstract class Result<A, E> {
+  abstract bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E>;
+  abstract toString(): string;
+}
+
+export class Ok<A, E> extends Result<A, E> {
+  value: A;
+  constructor(value: A) {
+    super();
+    this.value = value;
   }
 
-  class Ok<A, E> extends Result<A, E> {
-    value: A;
-    constructor(value: A) {
-      super();
-      this.value = value;
-    }
-
-    bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E> {
-      return f(this.value);
-    }
-
-    toString() {
-      return `Ok(${this.value})`;
-    }
+  bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E> {
+    return f(this.value);
   }
 
-  class Error<A, E> extends Result<A, E> {
-    value: E;
-    constructor(value: E) {
-      super();
-      this.value = value;
-    }
+  toString() {
+    return `Ok(${this.value})`;
+  }
+}
 
-    bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E> {
-      return new Error(this.value);
-    }
-
-    toString() {
-      return `Error(${this.value})`;
-    }
+class Error<A, E> extends Result<A, E> {
+  value: E;
+  constructor(value: E) {
+    super();
+    this.value = value;
   }
 
-  function fetchFailure(url: string): Result<string, FetchError> {
-    return new Error(new FetchError());
+  bind<B, E>(f: ((A) => Result<B, E>)): Result<B, E> {
+    return new Error(this.value);
   }
 
-  function fetchSuccess(url: string): Result<string, FetchError> {
-    return new Ok("abc");
+  toString() {
+    return `Error(${this.value})`;
   }
+}
 
-  class FetchError {}
+class FetchError {}
 
+function main() {
   console.log(
     fetchSuccess("someurl").bind(v => {
       return new Ok(v);
@@ -58,3 +50,13 @@ namespace Monads {
     })
   );
 }
+
+function fetchFailure(url: string): Result<string, FetchError> {
+  return new Error(new FetchError());
+}
+
+function fetchSuccess(url: string): Result<string, FetchError> {
+  return new Ok("abc");
+}
+
+main();
